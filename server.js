834b986e50a280d96311192fba2428b355ab4760
Chico <joshua.laroa13@gmail.com>
2021-05-27 00:13:20 +0800

    first committ admin

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = new express();
+const path = require('path');
+const session = require("express-session");
+const bodyparser = require("body-parser");
+const expressLayouts = require("express-ejs-layouts");
+
+// Require static assets from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set 'views' directory for any views 
+// being rendered res.render()
+app.set('views', path.join(__dirname, 'views'));
+
+// Set view engine as EJS
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static('public'));
+
+
+app.get('/',(req,res)=>{
+    res.render('Login.ejs');
+});
+app.get('/admin',(req,res)=>{
+    res.render('Admin.ejs');
+});
+app.get('/admin/products',(req,res)=>{
+    res.render('AdminProd.ejs');
+});
+
+
+app.listen(3000);
+console.log('Port Running on 3000');
